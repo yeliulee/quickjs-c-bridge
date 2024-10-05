882f7874b344bc032cf80a28088fd783325cb12e
yeliulee <yeliuleet@gmail.com>
2024-10-05 20:42:31 +0800

    optimize build script for ohos
    Signed-off-by: yeliulee <yeliuleet@gmail.com>

--- build-ohos.sh
+++ build-ohos.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Set your specific paths and variables here
-OHOS_TOOLCHAIN="CUSTOM_PATH_TO_OHOS_TOOLCHAIN_FILE"
+_SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# 使用环境变量和find命令动态查找toolchain文件
+if [ -z "$DEVECO_SDK_HOME" ]; then
+    echo "Error: DEVECO_SDK_HOME environment variable is not set."
+    exit 1
+fi
+
+OHOS_TOOLCHAIN=$(find "$DEVECO_SDK_HOME" -path "*/openharmony/*/cmake/ohos.toolchain.cmake" -print -quit)
+
+if [ -z "$OHOS_TOOLCHAIN" ]; then
+    echo "Error: Unable to find ohos.toolchain.cmake file."
+    exit 1
+fi
+
+echo "Using toolchain: $OHOS_TOOLCHAIN"
+
+# Set your specific variables here
 OHOS_ARCH="arm64-v8a"
 
 # clear previous build
@@ -17,4 +33,4 @@ cmake \
 # Build the project
 cmake --build ./build/ohos
 
-echo "build result located at ./build/ohos/libquickjs_c_bridge.so"
+echo "build result located at:\n $_SCRIPT_DIR/build/ohos/libquickjs_c_bridge.so"
